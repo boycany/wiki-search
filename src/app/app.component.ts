ba31898b8e56d539575ba45247fd9589d4ec9866
Red Chien <redchien@viridiasolutions.com>
2022-12-14 17:12:47 +0800

    done before RxJs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { WikipediaService } from './wikipedia.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'wiki-search';
+  pages = [];
+
+  constructor(private wikipedia: WikipediaService) {}
+
+  onTerm(term: string) {
+    // console.log('term from app.component :>> ', term);
+    // const results = this.wikipedia.search(term);
+    // console.log(results);
+    this.wikipedia.search(term).subscribe((response: any) => {
+      this.pages = response.query.search;
+    });
+  }
 }
+
+//typescript class 寫法
+// class Car {
+//   color: string;  //如果沒有初始化 color 的話，就要在 constructor 賦值
+
+//   constructor(receivedColor: string) {
+//     this.color = receivedColor;
+//   }
+// }
+
+// const myCar = new Car('red')
+// console.log(myCar.color)
